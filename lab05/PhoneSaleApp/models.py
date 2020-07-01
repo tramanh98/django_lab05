fa90d23b39bfbcc0431ea5005a2fad38c0a1f1fd
tramanhk11uit <tramanh1998tls@gmail.com>
2020-07-01 15:50:46 +0700

    add data

--- lab05/PhoneSaleApp/models.py
+++ lab05/PhoneSaleApp/models.py
@@ -7,7 +7,7 @@ from django.core.validators import MinValueValidator, MinLengthValidator
 class Brand (models.Model):
     idBrand = models.CharField(unique=True, primary_key=True, validators=[
                                MinLengthValidator(3, message="ID must have at least 3 characters")], max_length=20)
-    nameBrand = models.TextField()
+    nameBrand = models.CharField(max_length=200)
     descriptionBrand = models.TextField(default="This is a famous brand")
 
     def __str__(self):
@@ -17,7 +17,7 @@ class Brand (models.Model):
 class Product (models.Model):
     idProduct = models.CharField(unique=True, primary_key=True, max_length=20, validators=[
         MinLengthValidator(3, message="Id must have at least 3 characters")])
-    name = models.TextField(max_length=200)
+    name = models.CharField(max_length=200)
     description = models.TextField(default="This is a product")
     price = models.IntegerField(validators=[MinValueValidator(
         10000, message="The price must bigger than 10000")])
@@ -28,20 +28,20 @@ class Product (models.Model):
     idBrandProduct = models.ForeignKey(Brand, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.idProduct, self.idBrandProduct
-# model San Pham
+        return self.idProduct
+#model San Pham 
 # id
 # id Brand = Foreign Key of Hang
 # name
 # description
 # price
-# numberRemain
-# createdAt
-# updatedAt
-# avatar
+#numberRemain
+#createdAt
+#updatedAt
+#avatar
 
 
-# Model Hang
+#Model Hang
 # idBrand
-# nameBrand
-# descriptionBrand
+#nameBrand
+#descriptionBrand

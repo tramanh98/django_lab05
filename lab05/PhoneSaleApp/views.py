a51e3fba12363b25a804a6f33371c0df5821d9e5
WestlifeNo2 <WestlifeNo2@users.noreply.github.com>
2020-07-01 20:00:56 +0700

    Update views

--- lab05/PhoneSaleApp/views.py
+++ lab05/PhoneSaleApp/views.py
@@ -8,13 +8,40 @@ from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
 
 
 def index(request):
-    return render(request, "PhoneSaleApp/index.html")
+    first10Items = Product.objects.all()[:10]
+    return render(request, "PhoneSaleApp/index.html", first10Items)
 
 # input: id (param), output: render (request, url, json), JSON: y het model
 
 
+# chi tiết sản phẩm
+
+
 def detail(request):
-    return render(request, "PhoneSaleApp/ProductDetail.html")
+    # idPro = request.query_params.get("idProduct")
+    #   check = Product.objects.get(Product.idProduct=idPro)
+    #    if check != null:
+    return render(request, "PhoneSaleApp/PostProducts.html")
+    #     else:
+    # return HttpResponseBadRequest("No product, please add")
+
+
+# đăng tải bài đăng mới
+
+
+def post_product(request):
+    if request.method == "GET":
+        return render(request, "PhoneSaleApp/PostProducts.html")
+    else:
+        return HttpResponseBadRequest("No product, please add")
+    if request.method == "POST":
+        formProduct = productForm(request.data)
+        if formProduct.is_valid():
+            formProduct.save()
+            return HttpResponse("Success")
+        else:
+            return HttpResponseBadRequest("Invalid info, please try again")
+
 
 # post san pham moi: input: model san pham, output: json{success: true}
 # update san pham: input: nhu tren, output nhu tren

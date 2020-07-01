66b247741064c10bd287d53e8ce5dd50cf98a0b9
tramanhk11uit <tramanh1998tls@gmail.com>
2020-07-01 15:57:21 +0700

    update view

--- lab05/PhoneSaleApp/views.py
+++ lab05/PhoneSaleApp/views.py
@@ -7,17 +7,26 @@ from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
 # home: output: teen cac brand va 10 san pham moi nhat
 
 
+# Trang chủ
 def index(request):
     return render(request, "PhoneSaleApp/index.html")
 
 # input: id (param), output: render (request, url, json), JSON: y het model
 
-
+# chi tiết sản phẩm
 def detail(request):
     return render(request, "PhoneSaleApp/ProductDetail.html")
 
+# đăng tải bài đăng mới
 def post_product(request):
-    return render(request, "PhoneSaleApp/PostProducts.html")
+    if request.method == "GET":
+        return render(request, "PhoneSaleApp/PostProducts.html")
+    if request.method == "POST":
+        pass
+
+
+
+
 # post san pham moi: input: model san pham, output: json{success: true}
 # update san pham: input: nhu tren, output nhu tren
 # delete: input: id (param), output: json{success: true or false}
